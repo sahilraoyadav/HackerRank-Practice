cb9ad56cf83864a8f48f8e55fb35f970ddb222fb
sahilraoyadav <50744190+sahilraoyadav@users.noreply.github.com>
2021-05-02 04:13:29 -0400

    Update ArrayDS.java

--- ArrayDS.java
+++ ArrayDS.java
@@ -1,13 +1,28 @@
 import java.util.Arrays;
 import java.util.Collection;
-public class ArrayDS{
-    public static void main(String[] args) {
-        int[] test = {1,2,3};
-        int [] sTest = new int[test.length];
 
-        for(int i = 0; i < test.length; ++i){
-            sTest[i] = test[test.length-i-1];
+public class ArrayDS {
+    public static void main(String[] args) {
+        //Intializes the start array
+        int[] test = { 1, 2, 3 };
+        //Intializes null arrays to start with
+        int[] sTest = new int[test.length];
+        int[] aTest = new int[test.length];
+        //intializes vars to start with
+        int start = 0;
+        int counter = test.length - 1;
+        //Using While loop
+        while (counter >= 0) {
+            aTest[start] = test[counter];
+            start++;
+            counter--;
         }
-        System.out.println(Arrays.toString(sTest));   
+        //Using for loop
+        for (int i = 0; i < test.length; ++i) {
+            sTest[i] = test[test.length - i - 1];
+        }
+        //Prints out the Array
+        System.out.println(Arrays.toString(aTest));
+        System.out.println(Arrays.toString(sTest));
     }
 }
