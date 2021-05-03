0fe1cc9998fcf4bc47b83d6a833c8a3d632922d3
sahilraoyadav <50744190+sahilraoyadav@users.noreply.github.com>
2021-05-03 14:34:58 -0400

    a

---
+++ TwoDArrayDS.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+public class TwoDArrayDS {
+    public static void main(String[] args) {
+        final int[][] sArray = { { 1, 1, 1, 0, 0, 0 }, 
+                            { 0, 1, 0, 0, 0, 0 }, 
+                            { 1, 1, 1, 0, 0, 0 }, 
+                            { 0, 0, 2, 4, 4, 0 },              
+                            { 0, 0, 0, 2, 0, 0 },
+                            { 0, 0, 1, 2, 4, 0 } };
+        public static int[] a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16 = new int[6];
+        a1 = {sArray[0][0],sArray[0][1],sArray[0][2],sArray[1][1],sArray[2][0],sArray[2][1],sArray[2][2]};
+
+    }
+
+        
+    }
